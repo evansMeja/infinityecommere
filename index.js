a54da22d81ea2445e201002ca1bf9fae5f077ce1
Kiplagat Evans Kiprotich <35095785+evansMeja@users.noreply.github.com>
2018-07-16 00:04:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     // User is signed in.
 
-    document.getElementById("user_div").style.display = "block";
-    document.getElementById("login_div").style.display = "none";
+    //document.getElementById("user_div").style.display = "block";
+    //document.getElementById("login_div").style.display = "none";
 
-    var user = firebase.auth().currentUser;
+    //var user = firebase.auth().currentUser;
 
-    if(user != null){
+    //if(user != null){
 
-      var email_id = user.email;
-      document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
+     // var email_id = user.email;
+      //document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
+    window.location.href="www.programmingempire.com";
 
     }
 
